d28f487720520ac534bed8d824be801a6fe2e07e
carlosgallardo21 <52284997+carlosgallardo21@users.noreply.github.com>
2021-03-23 11:27:24 -0400

    Update hash.py

--- hash.py
+++ hash.py
@@ -3,3 +3,19 @@
 import csv
 import hashlib
 import os
+
+def main():
+  ...
+  
+def hash():
+  ...
+  
+def csv_parser(list):
+    csv_file = open("/tmp/hash.csv","a+")
+    filename = list[0]
+    filepath = list[1]
+    hash = list[2]
+    date = list[3]
+    csv_line = filename + ',' + filepath + ',' + hash + ',' + date
+    csv_file.write(csv_line)
+    csv_file.close()
