edf13445db91837841e3f40606110cbc2f173c4c
cheshire@CHESHIREwebserver.(none) <yolo@mail.com>
2021-03-29 23:13:23 -0400

    update

--- hash.py
+++ hash.py
@@ -31,20 +31,35 @@ def compHashFiles(initFile, recFile):
     
     return
 
+def csv_parser(list):
+    csv_file = open("hash.csv","a+")
+    filepath = list[0]
+    hashstring = list[1]
+    date = list[2]
+    csv_line = filepath + ',' + hashstring + ',' + date
+    csv_file.write(csv_line)
+    csv_file.close()
+    return
 
 def main():
-    excludeDir = ['/dev', '/proc', '/run', 'sys', '/tmp', '/var/lib', '/var/run']
+    excludeDir = ['/dev', '/proc', '/run', '/sys', '/tmp', '/var/lib', '/var/run']
 
     for subdir, dirs, files in os.walk(r'/'):
+        if subdir in excludeDir:
+            dirs[:]=[] # shallow copy, referenced from BeagleD
+            files[:]=[] # shallow copy, refernce from BeagleD
+            
         for filename in files:
             filepath = subdir + os.sep + filename
             
             hash = hashlib.sha256()
             hash.update(filepath.encode())
-            hashStr = hash.hexdigest())
-            hashtime = datetime.datetime.now()
+            hashStr = hash.hexdigest()
+            hashtime = str(datetime.datetime.now())
+            
             hashList = [filepath, hashtime, hashStr]
-
+            csv_parser(hashList)
+            
     return
 
 
