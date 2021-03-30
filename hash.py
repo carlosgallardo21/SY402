d528c2b21743088b8b1c3608486544daaa54f694
cheshire@CHESHIREwebserver.(none) <yolo@mail.com>
2021-03-30 02:16:25 -0400

    hash

--- hash.py
+++ hash.py
@@ -8,14 +8,6 @@ import difflib
 
 from os import path
 
-'''1. Hash files baseline
-    If hash file does not exist make the hash file
-2. If baseline exists, then we hash check all the files and then print out the difference
-'''
-'''FILE FORMAT'''
-'''filename, fullpath, hash, datetime'''
-
-'''Breakdown'''
 
 
 def csv_parser(inFlag, list):
@@ -67,12 +59,14 @@ def compHashFiles():
     newDict = {}
     with open(initFile, "r+") as finit:
         for init in finit.readlines():
-            oldDict[init[0]] = init[1]
+            line = init.split(',')
+            oldDict[line[0]] = line[2]
     finit.close()
 
-    with open(initFile, "r+") as frec:
+    with open(recFile, "r+") as frec:
         for rec in frec.readlines():
-            newDict[rec[0]] = rec[1]
+            lline = rec.split(',')
+            newDict[lline[0]] = lline[2]
     frec.close()
 
     oldFilePaths = oldDict.keys() #list of keys
@@ -83,12 +77,14 @@ def compHashFiles():
 
     for key in newFilePaths:
         if key not in oldFilePaths:
-            newList.append({key,newDict[key]})
+            print(key, newDict[key])
+            # newList.append(dict({key,newDict[key]}))
+            newList
         else:
             if oldDict[key] == newDict[key]:
                 continue
             else:
-                updateList.append({key,newDict[key]})
+                # updateList.append(dict({key,oldDict[key]}))
 
     if ((len(updateList) and len(newList)) == 0):
         print("NO FILE CHANGES")
