ec6dc4a1ddb917523aefd3f2a826f90019d0c1f2
carlosgallardo21 <52284997+carlosgallardo21@users.noreply.github.com>
2021-03-29 23:41:29 -0400

    Update csv_parser.py

--- csv_parser.py
+++ csv_parser.py
@@ -5,13 +5,12 @@ def main():
   list = ["filename","/tmp/","hash_here","datetime"]
   csv_parser(list)
 
-def csv_parser(list):
-    csv_file = open("hash.csv","wa+")
-    filepath = list[0]
-    hashstring = list[1]
-    date = list[2]
-    csv_line = filepath + ',' + hashstring + ',' + date
-    csv_file.write(csv_line)
-    csv_file.close()
 
+def csv_parser(list):
+  with open("hash.csv", 'a+') as csv_file:
+    csvwriter = csv.writer(csv_file)
+    csvwriter.writerow(list)
+    csv_file.close()
+    return
+    
 main()
