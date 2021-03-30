71738faf331b5e1581816e47fa3ac505f4942637
carlosgallardo21 <52284997+carlosgallardo21@users.noreply.github.com>
2021-03-30 01:18:56 -0400

    Create compare.py

---
+++ compare.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+newDict = {
+  "path1": "has1",
+  "path2": "has2",
+  "path4": "hash3",
+  "path5": "hash5"
+}
+oldFilePaths = oldDict.keys() #list of keys
+newFilePaths = newDict.keys() #list of keys
+oldvalue = oldDict.values()
+newvalue = newDict.values()
+newvalue = newDict.values()
+
+updateList = []
+newList = []
+
+for key in newFilePaths:
+        if key not in oldFilePaths:
+                newList.append({key,newDict[key]})
+        else:
+                if oldDict[key] == newDict[key]:
+                        continue
+                else:
+                        updateList.append({key,newDict[key]})
